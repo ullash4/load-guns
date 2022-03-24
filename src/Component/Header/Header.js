8f8964d213e1d1ec09d15c18ef86f5dc8e68d60e
ullash4 <programmerullash@gmail.com>
2022-03-24 22:26:31 +0600

    first commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <p>Gun Load</p>
+                li*3
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
